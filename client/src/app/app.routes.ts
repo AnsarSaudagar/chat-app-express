f755b0528b912ef65f51fef04660626b4a74f254
ansar <ansarsaudagar40@gmail.com>
2024-01-13 22:31:58 +0530

    added basic ui for card and added child routes for login signup

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 export const routes: Routes = [
-    {path: 'auth', component: AuthComponent}
+    {
+        path: 'auth', component: AuthComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'signup', component: SignupComponent }
+        ]
+    }
 ];
--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, RouterOutlet],
   providers: [AuthService],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
