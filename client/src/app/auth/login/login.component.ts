e1b96070e5457d368a7593639878e15325d6f2af
ansar <ansarsaudagar40@gmail.com>
2024-01-14 21:59:48 +0530

    basic form structure created for login signup

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm !: FormGroup;
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required])
+    })
+  }
+
+  onSubmit() { 
+    console.log(this.loginForm.value);
+    
+  }
 
 }
--- client/src/app/auth/signup/signup.component.ts
+++ client/src/app/auth/signup/signup.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupForm !: FormGroup;
 
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      'firstName': new FormControl(null, [Validators.required]) ,
+      'lastName': new FormControl(null) ,
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password': new FormControl(null, [Validators.required])
+    })
+  }
+
+  onSubmit() { 
+    console.log(this.signupForm.value);
+    
+  }
 }
