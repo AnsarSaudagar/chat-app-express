24d350292c06e0f7840121ab92d71e546b09eb48
ansar <ansarsaudagar40@gmail.com>
2024-01-23 23:07:27 +0530

    add files of home

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,24 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+<<<<<<< Updated upstream
 
 export const routes: Routes = [
     {path: 'auth', component: AuthComponent}
+=======
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
+
+export const routes: Routes = [
+    {
+        path: 'auth', component: AuthComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'signup', component: SignupComponent }
+        ]
+    },
+    {
+        path: 'home', component: HomeComponent
+    }
+>>>>>>> Stashed changes
 ];
---
+++ client/src/app/home/all-chats/all-chats.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-all-chats',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './all-chats.component.html',
+  styleUrl: './all-chats.component.css'
+})
+export class AllChatsComponent {
+
+}
