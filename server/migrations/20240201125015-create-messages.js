6678c8fb99d23d561096870e7738b13113adb787
ansar <ansarsaudagar40@gmail.com>
2024-02-01 18:26:43 +0530

    added messages table

---
+++ server/migrations/20240201125015-create-messages.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      conversation_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Conversations',
+          key: 'id'
+        }
+      },
+      sender_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      sent_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Messages');
+  }
+};
---
+++ server/models/messages.js
@@ -0,0 +1,40 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    static associate(models) {
+      Message.belongsTo(models.Conversation, { foreignKey: 'conversation_id' });
+      Message.belongsTo(models.User, { foreignKey: 'sender_id' });
+    }
+  }
+  Message.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    conversation_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    sender_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    sent_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Message',
+  });
+  return Message;
+};
